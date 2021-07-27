80ea1f9ec43b2e9ef276955f387655b9b198d2c6
Rgarbaciak <robin.garbaciak@gmail.com>
2021-07-27 15:22:23 +0200

    Ajout des données pour la BDD

---
+++ Firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+var fire = {
+  apiKey: "AIzaSyAVvm4iPx09Psa6sRicFodfcHKUKYJM9qk",
+  authDomain: "projet-medecine.firebaseapp.com",
+  projectId: "projet-medecine",
+  storageBucket: "projet-medecine.appspot.com",
+  messagingSenderId: "992104856393",
+  appId: "1:992104856393:web:b106e808263fd44699b898"
+    };
+
+export default firebase.initializeApp(fire) ;
