b155ed921dc14d7039dc58708b47cd17592c40ff
Rgarbaciak <robin.garbaciak@gmail.com>
2021-07-27 14:56:08 +0200

    Plein de salut

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import MenuNavigation from "./Navigation/MenuNavi";
-import { StatusBar } from "react-native";
+import { StatusBar, Text } from "react-native";
 export default function App() {
   return (
     
     <NavigationContainer>
       <StatusBar barStyle = "auto" hidden = {false}  translucent = {true}/>
+      <Text>Salut</Text>
+      <Text>Salut</Text>
+      <Text>Salut</Text>
+
+
+      <Text>Salut</Text>
+
   <MenuNavigation/>
     </NavigationContainer>
   );
